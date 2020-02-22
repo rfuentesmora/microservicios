e82f2f76cdb7c43297a4b35b055d90186264bdb1
rfuente <rene.fuentesmora@gmail.com>
2020-02-22 10:09:31 -0300

    servicio-eureka-server

---
+++ servicio-eureka-server/src/main/java/com/uy/curso/ServicioEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.uy.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class ServicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEurekaServerApplication.class, args);
+	}
+
+}
